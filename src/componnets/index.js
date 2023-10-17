3a0c900e5cb6aa963d2dc30eb1ce0e2518151016
ArqamSaleem4280 <arqamsaleem800@gmail.com>
2023-10-17 15:43:52 +0000

    Jira react

---
+++ src/componnets/index.js
@@ -0,0 +1,4 @@
+export{ default as Feed} from './Feed'
+export{ default as SideBar} from './SideBar'
+export{ default as StageCard} from './StageCard'
+export{ default as TicketCard} from './TicketCard'
